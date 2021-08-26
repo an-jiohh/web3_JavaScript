f4fcc4bb5bdabda7e758de5202bb2d61252ae29b
an-jiohh <dkswlgh99@naver.com>
2021-08-27 05:24:06 +0900

    HTML in Javascript

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,13 +1,6 @@
-const age = parseInt(prompt("how old are you?"));
+const title = document.getElementById("title");
 
-if(isNaN(age) || age<0){
-    console.log("Please write a real positive number");
-} else if(age < 18){
-    console.log("you are too young.");
-} else if(age >= 18 && age <= 50){
-    console.log("you can drink");
-} else if(age > 50 && age <= 80){
-    console.log("you should exercise");
-} else if(age > 80){
-    console.log("you can do whatever you want.");
-}
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
