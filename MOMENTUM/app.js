1425e040dbe753711d15aeb9b33965d7d032c631
an-jiohh <dkswlgh99@naver.com>
2021-08-27 04:29:37 +0900

    func

---
+++ MOMENTUM/app.js
@@ -0,0 +1,23 @@
+let a = 5;
+const b = 2;
+const myName = "nico";
+const days = ['mon','tue','wed'];
+const player = {
+    name: "jiho",
+    point: 10,
+};
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(days);
+console.log(days[0])
+console.log(player)
+console.log(player.name)
+
+function sayHello(nameOfPerson, age) {
+    console.log(nameOfPerson);
+}
+
+sayHello("nico", 10);
+sayHello();
