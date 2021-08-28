e01fabc76677f1277934e6ed33c0f2cb8e2917f6
an-jiohh <dkswlgh99@naver.com>
2021-08-28 22:48:41 +0900

    event listning

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,6 +1,38 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".Hello h1");
 
-title.innerText = "Got you!";
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
 
-console.log(title.id);
-console.log(title.className);
+function handelMouseEnter(){
+    h1.innerText = "mouse is here!";
+}
+
+function handelMouseleave(){
+    h1.innerText = "mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copy!");
+}
+
+function handelWindowOffline(){
+    alert("network is offline");
+}
+
+function handelWindowOnline(){
+    alert("network is online");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter",handelMouseEnter);
+h1.addEventListener("mouseleave",handelMouseleave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handelWindowOffline);
+window.addEventListener("online", handelWindowOnline);
