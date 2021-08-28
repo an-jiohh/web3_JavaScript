f109bfa3618c008683b017f0ea386825b6999aa6
an-jiohh <dkswlgh99@naver.com>
2021-08-28 23:06:35 +0900

    listning event merged with if

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,38 +1,15 @@
 const h1 = document.querySelector(".Hello h1");
 
 function handleTitleClick(){
-    h1.style.color = "blue";
-}
-
-function handelMouseEnter(){
-    h1.innerText = "mouse is here!";
-}
-
-function handelMouseleave(){
-    h1.innerText = "mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copy!");
-}
-
-function handelWindowOffline(){
-    alert("network is offline");
-}
-
-function handelWindowOnline(){
-    alert("network is online");
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newcolor = "tomato";
+    }
+    else{
+        newcolor = "blue";
+    }
+    h1.style.color = newcolor;    
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter",handelMouseEnter);
-h1.addEventListener("mouseleave",handelMouseleave);
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handelWindowOffline);
-window.addEventListener("online", handelWindowOnline);
